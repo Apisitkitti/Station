11b52f2a96db97ecd954445780cdd6d0d2052823
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2022-09-22 22:43:51 +0700

    whay

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+public class first
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Please input a number: ");
+        int station = int.Parse(Console.ReadLine());
+        if(station == 1)
+        {
+            Console.WriteLine("Cost (THB): 16");
+        }
+        else if(station == 2)
+        {
+            Console.WriteLine("Cost (THB): 23");
+        }
+        else if(station == 3)
+        {
+            Console.WriteLine("Cost (THB): 26");
+        }
+        else if(station == 4)
+        {
+            Console.WriteLine("Cost (THB): 30");
+        }
+        else if(station == 5)
+        {
+            Console.WriteLine("Cost (THB): 33");
+        }
+        else if(station == 6)
+        {
+            Console.WriteLine("Cost (THB): 37");
+        }
+        else if(station ==7)
+        {
+            Console.WriteLine("Cost (THB): 40");
+        }
+        else if(station>=8 && station<=15)
+        {
+            Console.WriteLine("Cost (THB): 44");
+        }
+        else if(station >15)
+        {
+            Console.WriteLine("Cost (THB): 59");
+        }
+    }
+}
